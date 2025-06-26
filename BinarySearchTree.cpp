1cb2bc08575fef81889beb09964940ce63331ecc
Nvann07 <nrizwardi@gmail.com>
2025-06-26 19:59:34 +0700

    Mencari posisi untuk menyispikan node baru

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,36 +1,40 @@
-//BinarySearchTree_0069
+// BinarySearchTree_0069
 
 #include <iostream>
 using namespace std;
 
-class Node 
+class Node
 {
+public:
   int info;
   Node *leftchild;
   Node *rightchild;
 
   // Constructor for the node class
-  Node() 
+  Node()
   {
-    leftchild = nullptr; // initialize left child to null
+    leftchild = nullptr;  // initialize left child to null
     rightchild = nullptr; // Initialize right child to null
   }
 };
 
 class BinaryTree
 {
-  public:
+public:
   Node *ROOT;
 
   BinaryTree()
   {
     ROOT = nullptr; // Initialize ROOT to null
-
   }
 
   void insert()
   {
-  
+    int x;
+    cout << "Masukkan nilai: ";
+    cin >> x;
 
-  }
-}
+    // Step 1: Alocate memery for the new node
+    Node *newNode = new Node();
+
+    
